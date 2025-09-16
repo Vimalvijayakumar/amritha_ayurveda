8844982bdaa0f12a8198ffdd96fd4d7227e69cac
Vimal <35964740+Vimalvijayakumar@users.noreply.github.com>
2025-09-16 23:27:24 +0530

    project structure

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff006837);
+  static const backgroundColor = Color(0xffFFFFFF);
+  static const textColor = Color(0xff404040);
+  static const inputBoxColor = Color(0xffD9D9D9);
+  static const grayColor = Color(0xffA4B5CA);
+}
